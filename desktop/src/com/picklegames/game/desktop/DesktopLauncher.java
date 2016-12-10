983d9b2610a240d43e604b039d9202fb50fe42b3
Miguel Garnica <mgarnica123@aol.com>
2016-12-09 23:10:40 -0800

    - Initial Commit

---
+++ desktop/src/com/picklegames/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.picklegames.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.picklegames.game.FishGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new FishGame(), config);
+	}
+}
